02e34825ecf9131c40b1b4f76e2678e44186a04b
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-02-01 22:15:37 +0700

    Routing

--- app/core/App.php
+++ app/core/App.php
@@ -2,6 +2,15 @@
 
 class App{
     public function __construct(){
-        echo"berhasil!";
+        $url = $this->parseUrl();
+        var_dump($url);
+    }
+
+    public function parseUrl(){
+        $url = $_GET["url"];
+        $url = rtrim($url, '/');
+        $url = filter_var($url, FILTER_SANITIZE_URL);
+        $url = explode('/', $url);
+        return $url;
     }
 }
